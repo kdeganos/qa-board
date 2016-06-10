92b0efabb81cf9245d5ed0efd37ff2d7e2e12831
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-06-10 10:58:25 -0700

    add new question functionality

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        questionAuthor: this.get('questionAuthor'),
+        questionContent: this.get('questionContent'),
+        notes: this.get('notes')
+      };
+        this.set('addNewQuestion', false);
+        this.sendAction('saveQuestion', params);
+      }
+  }
 });
